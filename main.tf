08f11ca66e91d1ac945b620220a0c9393fcc44f7
TEK Student <TEKstudent@vlab.lan>
2026-05-20 11:02:01 -0700

    Change environment tag from dev to development

--- main.tf
+++ main.tf
@@ -39,13 +39,13 @@ module "vpc" {
     environment = "Development"
   }
 }
-
+/*
 module "ec2_instances" {
   source = "./modules/aws-instance"
   instance_count = var.instance_count
   instance_type  = var.instance_type
 }
-
+*/
 module "app_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
   version = "3.17.0"
@@ -121,9 +121,10 @@ module "elb_http" {
 
 module "ec2_instances" {
   source = "./modules/aws-instance"
-
-  instance_count     = 2
-  instance_type      = "t2.micro"
+  instance_count = var.instance_count
+  instance_type  = var.instance_type
+  #instance_count     = 2
+  #instance_type      = "t2.micro"
   subnet_ids         = module.vpc.private_subnets[*]
   security_group_ids = [module.app_security_group.this_security_group_id]
 
